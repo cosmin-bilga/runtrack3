9b36d1d334f3aef3236e0bba3c6e49191a98ce23
cosmin-bilga <cosmin.bilga@laplateforme.io>
2025-10-17 13:23:47 +0200

    fix J2 J04

--- jour02/job04/script.js
+++ jour02/job04/script.js
@@ -3,6 +3,7 @@ document.addEventListener("keydown", function(event)
     const keylogger = document.getElementById("keylogger");
     console.log(`Key pressed: ${event.key}`);
     console.log(keylogger.value)
-    keylogger.value = keylogger.value + event.key;
+    if (event.key > "a" && event.key < "z" && event.key.length == 1)
+        keylogger.value = keylogger.value + event.key;
 
 });
