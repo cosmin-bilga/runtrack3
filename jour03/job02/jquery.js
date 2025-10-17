52410aa5bb272b99f71266a47ef7d6f8a84d4e4e
cosmin-bilga <cosmin.bilga@laplateforme.io>
2025-10-17 10:33:17 +0200

    jour03 job02

--- jour03/job02/jquery.js
+++ jour03/job02/jquery.js
@@ -1,5 +1,59 @@
-$('button').click(function() {
-    console.log("da");
-    const quote = $("<p>").text("Les logiciels et les cathédrales, c'est un peu la même chose - d'abord on les construit, ensuite on prie.");
-    $("body").append(quote);
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+function check_children()
+{
+    if ($("#placement-field").children().length === 6)
+    {
+        let prev_src = "arc0.png";
+        for (child of ($("#placement-field").children()))
+        {
+            let curr_src = child.src.split("/");
+            curr_src = curr_src[curr_src.length - 1];
+            //console.log(prev_src, curr_src);
+            //console.log(prev_src > curr_src);
+            if (prev_src > curr_src)
+            {
+                let p = $("<p>").text("Vous avez perdu");
+                p.css("color","red");
+                $("header").append(p);
+                return;
+            }
+            prev_src = curr_src;
+            //if (child.src())
+        }
+        let p = $("<p>").text("Vous avez gagné");
+        p.css("color","green");
+        $("header").append(p);
+    }
+}
+
+
+$("#reset").click(function(){
+    $("#placement-field").empty();
+    $("header").empty();
+    const order = shuffle([1,2,3,4,5,6]);
+    //console.log(order);
+    for (let item of order)
+    {
+        //console.log(item);
+        let x = $("<img>").attr("src","arc" + +(+item) + ".png");
+        x.attr("class","arc");
+        $("#random-box").append(x);
+    }
+    //$("#random-box").appendChild()
 });
+
+
+$("#random-box").on("click",".arc", function(){ 
+    $("#placement-field").append(this);
+    check_children();
+})
+
+
+
