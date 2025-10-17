a1a1ecfed09fca3cc643caa92f3e68867ee6f3ed
cosmin-bilga <cosmin.bilga@laplateforme.io>
2025-10-17 13:25:10 +0200

    z

--- jour02/job04/script.js
+++ jour02/job04/script.js
@@ -3,7 +3,7 @@ document.addEventListener("keydown", function(event)
     const keylogger = document.getElementById("keylogger");
     console.log(`Key pressed: ${event.key}`);
     console.log(keylogger.value)
-    if (event.key > "a" && event.key < "z" && event.key.length == 1)
+    if (event.key >= "a" && event.key <= "z" && event.key.length == 1)
         keylogger.value = keylogger.value + event.key;
 
 });
